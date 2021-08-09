69db80e40d8ace7f544ed1c4f67003418f1512cf
Okiri Gabriel <okirigabriel24@gmail.com>
2021-08-09 06:58:35 -1000

    footer and header done also restreudtedbtheciode

---
+++ assets/js/pages/custom.js
@@ -0,0 +1,43 @@
+// const Hello = () => {
+//   // eslint-disable-next-line no-console
+//   console.log('hello');
+// };
+
+// export default Hello;
+
+AOS.init({
+    easing: 'ease-out-back',
+    duration: 1000
+    });
+  
+  
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
+  })
+  
+  function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
