d3ff99fe29907c0e1f5cd5b6f7b01dba6e306e32
Okiri Gabriel <okirigabriel24@gmail.com>
2021-08-11 17:46:48 -1000

    ...

--- assets/js/pages/custom.js
+++ assets/js/pages/custom.js
@@ -1,32 +1,12 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-AOS.init({
-  easing: 'ease-out-back',
-  duration: 1000,
-});
-
-$('.owl-carousel').owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  responsive: {
-    0: {
-      items: 1,
-    },
-    600: {
-      items: 3,
-    },
-    1000: {
-      items: 5,
-    },
-  },
-});
 
 function openCity(evt, cityName) {
-  let i; let tabcontent; let
-    tablinks;
+  let i; let tabcontent; 
+  let tablinks;
   tabcontent = document.getElementsByClassName('tabcontent');
+  
   for (i = 0; i < tabcontent.length; i += 1) {
     tabcontent[i].style.display = 'none';
   }
